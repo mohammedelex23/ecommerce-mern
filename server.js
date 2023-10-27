49160e31e3685688b0f874ff778da62058779865
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-10-27 20:03:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "https://checkout.stripe.com",
-      "https://ecommerce-mern-khfhryear-mohammedelex23.vercel.app",
+      "https://ecommerce-mern-jet-one.vercel.app",
     ],
     methods: ["GET", "POST", "PUT"],
   })
